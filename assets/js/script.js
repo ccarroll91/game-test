cc9000e0c5ee55b6987a9d186402b4be8eaefa5c
ccarroll91 <conor-carroll@hotmail.com>
2021-03-16 11:08:16 +0000

    l

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,4 @@
-/*-------------------------------------------------Carousel---*/
 
-$(document).ready(function(){
-      
-  $("#myCarousel").carousel({wrap: false});
-
-  $(".left").click(function(){
-    $("#myCarousel").carousel("prev");
-  });
-  $(".right").click(function(){
-    $("#myCarousel").carousel("next");
-  });
-});
-
-console.log("myCarousel")
-
-$('#myCarousel').on('slid', '', checkitem);
-$('#myCarousel').on('slid.bs.carousel', '', checkitem);
-
-$(document).ready(function(){
-    checkitem();
-});
 
 /*---------------------------------------------------------------------dropdown*/
 
@@ -45,6 +24,74 @@ window.onclick = function(event) {
 
 /*---------------------------------------------------------------------about----*/
 
+
+
+/*--------------------------------------------------Fullscreen--*/
+
+
+/*---------------------------------------------------audio*/
+/*---- A simple audio function that allows the user to click on the chosen image and play an audio file--*/
+/*---- Format for inserting audio into HTML element is - onclick="playAudio('#.mp3')"---*/
+
+$(document).ready(function(){
+ 
+  $("#myCarousel").carousel({wrap: false});
+    
+ 
+
+    
+  
+  $(".left").click(function(){
+    $("#myCarousel").carousel("prev");
+  });
+  $(".right").click(function(){
+    $("#myCarousel").carousel("next");
+  });
+});
+
+console.log("myCarousel")
+
+$('#myCarousel').on('slid', '');
+$('#myCarousel').on('slid.bs.carousel', '');
+
+$(document).ready(function(){
+    checkitem();
+});
+
+function checkitem()
+{
+var $this = $('#myCarousel');
+    if($('.carousel-inner .item:first').hasClass('active')) {
+        $this.children('.left.carousel-control').hide();
+        $this.children('.right.carousel-control').show();
+    } else if($('.carousel-inner .item:last').hasClass('active')) {
+        $this.children('.left.carousel-control').show();
+        $this.children('.right.carousel-control').hide();
+    } else {
+        $this.children('.carousel-control').show();
+    } 
+}
+
+var elem = document.getElementById("myCarousel");
+function openFullscreen() {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.webkitRequestFullscreen) { /* Safari */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE11 */
+    elem.msRequestFullscreen();
+  }
+}
+
+
+
+/*----------------------------------------------email-------*/
+
+
+
+
+/*---------------------------------------------------------------------about----*/
+
 document.querySelector('#about-button').addEventListener('click', toggle)
 
 function toggle(event) {
@@ -56,21 +103,3 @@ function toggle(event) {
     document.getElementById('about').style.display = 'none'
   }
 }
-
-/*--------------------------------------------------Fullscreen--*/
-
-var elem = document.getElementById("myCarousel");
-function openFullscreen() {
-  if (elem.requestFullscreen) {
-    elem.requestFullscreen();
-  } else if (elem.webkitRequestFullscreen) { 
-    elem.webkitRequestFullscreen();
-  } else if (elem.msRequestFullscreen) { 
-    elem.msRequestFullscreen();
-  }
-}
-
-/*---------------------------------------------------audio*/
-/*---- A simple audio function that allows the user to click on the chosen image and play an audio file--*/
-/*---- Format for inserting audio into HTML element is - onclick="playAudio('#.mp3')"---*/
-
