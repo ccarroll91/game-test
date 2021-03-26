3f69d2ed756d4988600d8e09e82a2c7ee88f3444
ccarroll91 <conor-carroll@hotmail.com>
2021-03-26 13:23:37 +0000

    insert array

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,7 +30,34 @@ $(document).ready(function(){
     $("#myCarousel").carousel("next");
   });
 });
-
+var i = 0, imgsrc = new Array(), preload = new Array();
+imgsrc[0]="assets/images/titleplaceholder.png";
+imgsrc[1]="assets/images/1.png";
+imgsrc[2]="assets/images/2.png";
+imgsrc[3]="assets/images/3.png";
+imgsrc[4]="assets/images/4.png";
+imgsrc[5]="assets/images/5.png";
+imgsrc[6]="assets/images/6.png";
+imgsrc[7]="assets/images/7.png";
+imgsrc[8]="assets/images/8.png";
+imgsrc[9]="assets/images/9.png";
+imgsrc[0]="assets/images/10.png";
+imgsrc[11]="assets/images/11.png";
+imgsrc[12]="assets/images/12.png";
+imgsrc[13]="assets/images/13.png";
+imgsrc[14]="assets/images/14.png";
+imgsrc[15]="assets/images/15.png";
+imgsrc[16]="assets/images/16.png";
+imgsrc[17]="assets/images/17.png";
+imgsrc[18]="assets/images/18.png";
+imgsrc[19]="assets/images/19.png";
+imgsrc[20]="assets/images/20.png";
+imgsrc[21]="assets/images/21.png";
+imgsrc[22]="assets/images/22.png";
+imgsrc[23]="assets/images/23.png";
+imgsrc[24]="assets/images/24.png";
+imgsrc[25]="assets/images/25.png";
+imgsrc[26]="assets/images/creditsplaceholder.png"
 
 function mode(param)
 {
@@ -45,7 +72,7 @@ document.getElementById("pause").disabled="";
 document.getElementById("stop").disabled="";
 document.getElementById("slideshow").src=imgsrc[i];
 i++;
-
+setTimeout("startSlideshow()",1000);
 }
 else if(smode=="pause")
 {
