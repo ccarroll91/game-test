c5d10191b1c4aa4e2cdfd15e8f9b751e9c0ba4ac
ccarroll91 <conor-carroll@hotmail.com>
2021-03-26 13:12:31 +0000

    autoplay test

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,22 +23,15 @@ $(document).ready(function(){
 
     
   
-  
+  $(".left").click(function(){
+    $("#myCarousel").carousel("prev");
+  });
+  $(".right").click(function(){
+    $("#myCarousel").carousel("next");
+  });
+});
 
 
-var imgsrc = new Array();
-imgsrc[0]="assets/images/titleplaceholder.png";
-imgsrc[1]="assets/images/1.png, assets/audio/audio2.mp3";
-imgsrc[2]="assets/images/2.png";
-imgsrc[3]="assets/images/3.png";
-imgsrc[4]="assets/images/4.png";
-imgsrc[5]="assets/images/5.png";
-
-for (var j=0;j<imgsrc.length;j++)
-{
-preload[j] = new Image;
-preload[j].src = imgsrc[j];
-}
 function mode(param)
 {
 smode=param;
@@ -52,7 +45,7 @@ document.getElementById("pause").disabled="";
 document.getElementById("stop").disabled="";
 document.getElementById("slideshow").src=imgsrc[i];
 i++;
-setTimeout("startSlideshow()",3000);
+
 }
 else if(smode=="pause")
 {
@@ -66,7 +59,7 @@ document.getElementById("play").disabled="";
 document.getElementById("play").value="Play";
 document.getElementById("pause").disabled="disabled";
 document.getElementById("stop").disabled="disabled";
-document.getElementById("slideshow").src=imgsrc[0];
+document.getElementById("myCarousel").src=imgsrc[0];
 i=0;
 }
 if(i==imgsrc.length)
@@ -75,10 +68,4 @@ i=0;
 }
 }
 
-$(".left").click(function(){
-    $("#slideshow").slide("prev");
-  });
-  $(".right").click(function(){
-    $("#slideshow").slide("next");
-  });
-});
+
