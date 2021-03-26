550e0b3fdebec6ba66408d2acc7000c029544a09
ccarroll91 <conor-carroll@hotmail.com>
2021-03-26 12:54:56 +0000

    slide test

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,112 +23,62 @@ $(document).ready(function(){
 
     
   
-  $(".left").click(function(){
-    $("#myCarousel").carousel("prev");
+  
+
+
+var imgsrc = new Array();
+imgsrc[0]="assets/images/titleplaceholder.png";
+imgsrc[1]="assets/images/1.png, assets/audio/audio2.mp3";
+imgsrc[2]="assets/images/2.png";
+imgsrc[3]="assets/images/3.png";
+imgsrc[4]="assets/images/4.png";
+imgsrc[5]="assets/images/5.png";
+
+for (var j=0;j<imgsrc.length;j++)
+{
+preload[j] = new Image;
+preload[j].src = imgsrc[j];
+}
+function mode(param)
+{
+smode=param;
+}
+function startSlideshow()
+{
+if(smode=="play")
+{
+document.getElementById("play").disabled="disabled";
+document.getElementById("pause").disabled="";
+document.getElementById("stop").disabled="";
+document.getElementById("slideshow").src=imgsrc[i];
+i++;
+setTimeout("startSlideshow()",3000);
+}
+else if(smode=="pause")
+{
+document.getElementById("pause").disabled="disabled";
+document.getElementById("play").disabled="";
+document.getElementById("play").value="Resume";
+}
+else if(smode=="stop")
+{
+document.getElementById("play").disabled="";
+document.getElementById("play").value="Play";
+document.getElementById("pause").disabled="disabled";
+document.getElementById("stop").disabled="disabled";
+document.getElementById("slideshow").src=imgsrc[0];
+i=0;
+}
+if(i==imgsrc.length)
+{
+i=0;
+}
+}
+
+$(".left").click(function(){
+    $("#slideshow").slide("prev");
   });
   $(".right").click(function(){
-    $("#myCarousel").carousel("next");
+    $("#slideshow").slide("next");
   });
 });
-
-setInterval(() => {
-     $("#myCarousel").carousel("next");
-     
-
-
-    // Write code to play audio
-  }, 3000);
-
-
-$('#myCarousel').on('slid.bs.carousel', checkitem);
-
-$(document).ready(function(){
-    checkitem();
-});
-
-function checkitem()
-{
-var $this = $('#myCarousel');
-    if ($('.carousel-inner .item:first').hasClass('active')) {
-        $this.children('.left.carousel-control').hide();
-        $this.children('.right.carousel-control').secret();
-    } else if ($('.carousel-inner .item:last').hasClass('active')) {
-        $this.children('.right.carousel-control').hide();
-        $this.children('.left.carousel-control').secret();
-    } else {
-        $this.children('.carousel-control').secret();
-    }
-}
-
-var elem = document.getElementById("myCarousel");
-function openFullscreen() {
-  if (elem.requestFullscreen) {
-    elem.requestFullscreen();
-  } else if (elem.webkitRequestFullscreen) { /* Safari */
-    elem.webkitRequestFullscreen();
-  } else if (elem.msRequestFullscreen) { /* IE11 */
-    elem.msRequestFullscreen();
-  }
-}
-
-document.querySelector('#secret-btn').addEventListener('click', toggle)
-
-function toggle(event) {
-    if (document.getElementById('secret').style.display == 'none') {
-    event.target.innerText = 
-    `Want to know a secret?`
-    document.getElementById('secret').style.display = ''
-  } else {
-    event.target.innerText = 'Want to know a secret?'
-    document.getElementById('secret').style.display = 'none'
-  }
-}
-
-
-/*
-    Stop / Start carousel autoplay
-*/
-$('.btn-customized').on('click', function(){
- 
-    if( ! $(this).hasClass('paused') ) {
-        $('.carousel').mycarousel('pause');
-        $('.btn-customized').toggleClass('paused');
-        $('.btn-customized i').removeClass('fa-pause').addClass('fa-play');
-        $(this).blur();
-    }
-    else {
-        $('.carousel').carousel('cycle');
-        $('.btn-customized').toggleClass('paused');
-        $('.btn-customized i').removeClass('fa-play').addClass('fa-pause');
-        $(this).blur();
-    }
- 
-});
-
-
-var slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-}
