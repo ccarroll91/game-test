74c38694b34bc94dd8663254794b46ba5da7e1f7
ccarroll91 <conor-carroll@hotmail.com>
2021-07-08 09:59:20 +0000

    Updated files to match original, adding new audio line to try stop loop

--- assets/js/audio.js
+++ assets/js/audio.js
@@ -137,6 +137,9 @@ document.getElementById('play1').addEventListener('click', function (e) {
   
   /*--------------------------------------------------audio stop--*/
 
-var sound = document.getElementById("audio");
-    sound.pause();
-    sound.currentTime = 0;
+  document.addEventListener('play', function(e){
+    var audios = document.getElementsByTagName('audio');
+    for(var i = 0, len = audios.length; i < len;i++){
+        if(audios[i] != e.target){
+            audios[i].pause();
+        }} 
